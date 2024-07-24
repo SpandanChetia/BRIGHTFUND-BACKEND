8d32f78e317c7367f393a559d05d454332aff8bf
SpandanPriyam22 <spandanpriyam1976@gmail.com>
2024-07-25 01:13:52 +0530

    Files added

--- app.js
+++ app.js
@@ -5,12 +5,19 @@ import { application } from "express";
 import { graphqlHTTP } from "express-graphql";
 import graphqlSchema from "./graphql/schema";
 import graphqlResolver from "./graphql/resolvers";
+import path from "path";
+
+const authRoutes = require("./routes/auth");
 
 const app = express();
 
 app.set("view engine", "pug");
-app.set("view", "views");
+app.set("views", "views");
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(authRoutes);
 
 app.use(
   "/graphql",
@@ -18,23 +25,25 @@ app.use(
     schema: graphqlSchema,
     rootValue: graphqlResolver,
     graphiql: true,
-    formatError(err){
-      if(!err.originalError){
+    formatError(err) {
+      if (!err.originalError) {
         return err;
       }
       const data = err.originalError.data;
-      const message = err.message || 'An error occured.';
+      const message = err.message || "An error occured.";
       const code = err.originalError.code || 500;
-      return { message: message, status: code, data : data};
-    }
+      return { message: message, status: code, data: data };
+    },
   })
 );
 
-mongoose.connect('DATABASE_URL')
-  .then(result=>{
-    app.listen(3000);
+mongoose
+  .connect("DATABASE_URL", { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    app.listen(3000, () => {
+      console.log("Server is running on port 3000");
+    });
   })
-  .catch(err=>{
-    console.log('Error')
+  .catch((err) => {
+    console.error("Database connection error:", err);
   });
-
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,40 +1,40 @@
-import User from '../models/user';
-import bcrypt from 'bcryptjs';
+import User from "../models/user";
+import bcrypt from "bcryptjs";
 
-exports.getSignup = (req, res, next) =>{
-    res.render('signup', {
-        path: '/signup',
-        pageTitle: 'Create Account',
-        isAuthenticated: false
-    })
-}
+exports.getSignup = (req, res, next) => {
+  res.render("signup", {
+    path: "/signup",
+    pageTitle: "Create Account",
+    isAuthenticated: false,
+  });
+};
 
-exports.postSignup = (req, res, next) =>{
-    const fullName = req.body.fullName;
-    const email = req.body.email;
-    const password = req.body.password;
-    const confirmpassword = req.body.confirmPassword;
-    User.findOne({email: email})
-    .then(userDoc=>{
-        if(userDoc) {
-            return res.redirect('/signup');
-        }
-        return bcrypt
+exports.postSignup = (req, res, next) => {
+  const fullName = req.body.fullName;
+  const email = req.body.email;
+  const password = req.body.password;
+  const confirmpassword = req.body.confirmPassword;
+  User.findOne({ email: email })
+    .then((userDoc) => {
+      if (userDoc) {
+        return res.redirect("/signup");
+      }
+      return bcrypt
         .hash(password, 12)
-        .then(hashedPassword=>{
-            const user = new User({
-                fullName: fullName,
-                email: email,
-                password: hashedPassword,
-                fundraisers: []
-            });
-            return user.save();
+        .then((hashedPassword) => {
+          const user = new User({
+            fullName: fullName,
+            email: email,
+            password: hashedPassword,
+            fundraisers: [],
+          });
+          return user.save();
         })
-        .then(result =>{
-            res.redirect('/signin');
-        })
+        .then((result) => {
+          res.redirect("/signin");
+        });
     })
-    .catch(err=>{
-        console.log(err);
+    .catch((err) => {
+      console.log(err);
     });
 };
